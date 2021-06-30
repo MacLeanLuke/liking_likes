718753a11ee8e4e3387d4e1ea243642c900fbd72
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2021-06-30 09:34:08 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var countElement1 = document.querySelector("#count1");
+var count1 = 0;
+
+function add1(){
+    count1++;
+    countElement1.innerText = `${count1} like(s)`
+    // console.log(count);
+}
+
+var countElement2 = document.querySelector("#count2");
+var count2 = 0;
+
+function add2(){
+    count2++;
+    countElement2.innerText = `${count2} like(s)`
+    // console.log(count);
+}
+
+var countElement3 = document.querySelector("#count3");
+var count3 = 0;
+
+function add3(){
+    count3++;
+    countElement3.innerText = `${count3} like(s)`
+    // console.log(count);
+}
